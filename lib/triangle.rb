dafc01221a459231240249d6e3f4adb7c7d0d57c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-21 15:05:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
+#require 'pry'
 class Triangle
   # write code here
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    #examples: 2, 3, 4
+    sides = [@side1, @side2, @side3]
+    uniques = sides.uniq
+    num_of_uniques = uniques.size
+    sorted_sides = sides.sort
+    #sum = sorted_sides.inject(0){|sum, x| sum + x}
+    # sum/3 == sorted_sides[0]
+    #binding.pry
+    if sorted_sides[0] + sorted_sides[1] <= sorted_sides[2] || sides.any? {|n| n <= 0}
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        error.message
+      end
+    elsif num_of_uniques == 1
+      return :equilateral
+    elsif num_of_uniques == 2
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid dimensions."
+    end
+  end
 end
+puts Triangle.new(0,0,0).kind
